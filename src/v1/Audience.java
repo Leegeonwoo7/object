3031dc4cd80f4013abc8998516d971075dfd44f3
Leegeonwoo7 <workgeon07@naver.com>
2024-10-29 16:48:59 +0900

    Audience에서 Bag을 수동적으로 사용하는 것을 수정
    이전의 Bag은 자율적이지 않고 Audience에 의해서 수동적으로 호출당했었다
    Bag을 자율적이고 독립적인 객체로 수정한다

--- src/v1/Audience.java
+++ src/v1/Audience.java
@@ -7,18 +7,7 @@ public class Audience {
         this.bag = bag;
     }
 
-    public Bag getBag() {
-        return bag;
-    }
-
     public long buy(Ticket ticket) {
-        if (bag.hasInvitation()) {
-            getBag().setTicket(ticket);
-            return 0L;
-        } else {
-            bag.minusAmount(ticket.getFee());
-            bag.setTicket(ticket);
-            return ticket.getFee();
-        }
+        return bag.hold(ticket);
     }
 }
